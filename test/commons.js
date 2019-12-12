7db83d4fd830d1ac8670ce153d87134041a77df3
lucas jin <lucas1004jx@gmail.com>
2019-12-12 23:48:29 +0100

    To fail greet function test

--- test/commons.js
+++ test/commons.js
@@ -1,13 +1,13 @@
 const expect = require('expect.js');
 const { reverseStr, isBetween, daytime, isPalindrome } = require('../lib/commons');
 
-describe('commons', () => {
+describe('it tests commons functions', () => {
 
   it('should reverse the string', () => {
     const str = 'hola';
     const expected = 'aloh';
     const result = reverseStr(str);
-    expect(expected).to.equal(result);
+    expect(result).to.equal(expected);
   });
 
   it('should test a number is within a range', () => {
@@ -15,21 +15,21 @@ describe('commons', () => {
     const range = [0,12];
     const expected = true;
     const result = isBetween(num, range);
-    expect(expected).to.equal(result);
+    expect(result).to.equal(expected);
   });
 
   it('should return the daytime according the hour', () => {
     const hours = 8;
     const expected = 'morning';
     const result = daytime(hours);
-    expect(expected).to.equal(result);
+    expect(result).to.equal(expected);
   });
 
   it('should test a palindrome word', () => {
     const word = 'oso';
     const expected = true;
     const result = isPalindrome(word);
-    expect(expected).to.equal(result);
+    expect(result).to.equal(expected);
   });
 
 });
--- test/strings.js
+++ test/strings.js
@@ -1,7 +1,12 @@
 const expect = require('expect.js');
+const { greet, goodbye } = require('../lib/strings');
 
-describe('strings', () => {
-  it('should do tests', () => {
-    expect(null).to.equal(null);
+describe('it tests strings functions', () => {
+  it('should greet user Good morning', () => {
+    const user = 'lucas';
+    const hours = 18;
+    const expected = `Good morning, ${user}!`;
+    const result = greet(user, hours);
+    expect(result).to.equal(expected);
   });
 });
