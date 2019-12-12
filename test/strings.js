5bef570d8e053f32c2cbbf56329499ab98f4b5b3
lucas jin <lucas1004jx@gmail.com>
2019-12-12 23:56:04 +0100

    To fail goodbye function test

--- test/strings.js
+++ test/strings.js
@@ -2,27 +2,39 @@ const expect = require('expect.js');
 const { greet, goodbye } = require('../lib/strings');
 
 describe('it tests strings functions', () => {
-  it('should greet user Good morning', () => {
-    const user = 'lucas';
-    const hours = 8;
-    const expected = `Good morning, lucas!`;
-    const result = greet(user, hours);
-    expect(result).to.equal(expected);
-  });
+  describe('it tests greet function', () => {
+    it('should greet user Good morning', () => {
+      const user = 'lucas';
+      const hours = 8;
+      const expected = `Good morning, lucas!`;
+      const result = greet(user, hours);
+      expect(result).to.equal(expected);
+    });
 
-  it('should greet user Good morning', () => {
-    const user = 'lucas';
-    const hours = 15;
-    const expected = `Good afternoon, lucas!`;
-    const result = greet(user, hours);
-    expect(result).to.equal(expected);
-  });
+    it('should greet user Good morning', () => {
+      const user = 'lucas';
+      const hours = 15;
+      const expected = `Good afternoon, lucas!`;
+      const result = greet(user, hours);
+      expect(result).to.equal(expected);
+    });
 
-  it('should greet user Good morning', () => {
-    const user = 'lucas';
-    const hours = 22;
-    const expected = `Good night, lucas!`;
-    const result = greet(user, hours);
-    expect(result).to.equal(expected);
-  });
+    it('should greet user Good morning', () => {
+      const user = 'lucas';
+      const hours = 22;
+      const expected = `Good night, lucas!`;
+      const result = greet(user, hours);
+      expect(result).to.equal(expected);
+    });
+  })
+
+  describe('it tests goodbye function', () => {
+    it('should say goodbye to user', () => {
+      const user = 'Pablo';
+      const expected = 'Goodbye lucas!';
+      const result = goodbye(user);
+      expect(result).to.equal(expected);
+    });
+  })
+
 });
