04e58397a5bdef8760c6a052d005ff35145ec0ee
lucas jin <lucas1004jx@gmail.com>
2019-12-12 23:50:34 +0100

    To fail greet function test

--- test/strings.js
+++ test/strings.js
@@ -5,7 +5,7 @@ describe('it tests strings functions', () => {
   it('should greet user Good morning', () => {
     const user = 'lucas';
     const hours = 18;
-    const expected = `Good morning, ${user}!`;
+    const expected = `Good morning, lucas1!`;
     const result = greet(user, hours);
     expect(result).to.equal(expected);
   });
@@ -13,7 +13,7 @@ describe('it tests strings functions', () => {
   it('should greet user Good morning', () => {
     const user = 'lucas';
     const hours = 22;
-    const expected = `Good afternoon, ${user}!`;
+    const expected = `Good afternoon, lucas2!`;
     const result = greet(user, hours);
     expect(result).to.equal(expected);
   });
@@ -21,7 +21,7 @@ describe('it tests strings functions', () => {
   it('should greet user Good morning', () => {
     const user = 'lucas';
     const hours = 8;
-    const expected = `Good night, ${user}!`;
+    const expected = `Good night, lucas3!`;
     const result = greet(user, hours);
     expect(result).to.equal(expected);
   });
