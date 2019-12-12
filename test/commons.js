4120c836df6ea2ceb003c84ab27855089d470258
lucas jin <lucas1004jx@gmail.com>
2019-12-12 23:24:35 +0100

    To fail isBetween function test

--- test/commons.js
+++ test/commons.js
@@ -1,11 +1,21 @@
 const expect = require('expect.js');
-const { reverseStr } = require('../lib/commons');
+const { reverseStr, isBetween } = require('../lib/commons');
 
 describe('commons', () => {
+
   it('should reverse the string', () => {
     const str = 'hola';
     const expected = 'aloh';
     const result = reverseStr(str);
     expect(expected).to.equal(result);
   });
+
+  it('should test a number is within a range', () => {
+    const num= 14;
+    const range = [0,12];
+    const expected = true;
+    const result = isBetween(num, range)
+    expect(expected).to.equal(result);
+  });
+
 });
