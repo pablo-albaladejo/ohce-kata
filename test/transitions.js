bf107344e8b15692bfd2ff578a154b95a5351720
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-12-13 11:02:47 +0100

    transitions red: to fail stateTransition function

--- test/transitions.js
+++ test/transitions.js
@@ -1,7 +1,35 @@
 const expect = require('expect.js');
 
-describe('transitions', () => {
-  it('should do tests', () => {
-    expect(null).to.equal(null);
-  });
+const { STATES } = require('../lib/state/states');
+const {stateTransition } = require('../lib/state/transitions');
+
+describe('it tests state transitions', () => {
+
+  const S0 = {
+    q: STATES.INIT,
+    attrs: {
+      hours: null,
+      name: null
+    }
+  }
+
+  describe('stateTransition should return a nextState and an output', () => {
+    const hours = (new Date()).getHours();
+    const result = stateTransition(S0, hours);
+
+    it('should return a result', () => {
+      expect(result).not.to.equal(null);
+    });
+
+    it('should return a nextState', () => {
+      expect(result.nextState).not.to.equal(null);
+    });
+
+    it('should return a output', () => {
+      expect(result.output).not.to.equal(null);
+    });
+
+  })
+
+
 });
