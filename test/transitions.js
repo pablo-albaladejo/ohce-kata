679f9a5e926f67ce97704f1c3cd9d023cc2d10c9
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-12-13 12:07:15 +0100

    name state transitions: red failing tests

--- test/transitions.js
+++ test/transitions.js
@@ -2,6 +2,7 @@ const expect = require('expect.js');
 
 const { STATES } = require('../lib/state/states');
 const { stateTransition } = require('../lib/state/transitions');
+const { greet } = require('../lib/strings');
 
 describe('it tests state transitions', () => {
 
@@ -39,22 +40,55 @@ describe('it tests state transitions', () => {
   })
 
   describe('transitions from INIT', () => {
-    
+
     const hours = (new Date()).getHours();
     const { nextState, output } = stateTransition(S0, hours);
-  
+
     it('should transitioning from INIT state to NAME state', () => {
       expect(nextState.q).to.equal(STATES.NAME);
     });
-  
+
     it('should set the hours', () => {
       expect(nextState.attrs.hours).to.equal(hours);
     });
-  
+
     it('should have no output', () => {
       expect(output.length).to.equal(0);
     });
-  
+
+  });
+
+  describe('transitions from NAME', () => {
+
+    const hours = 0;
+    const S1 = {
+      q: STATES.NAME,
+      attrs: {
+        hours,
+        name: null
+      }
+    }
+
+    const name = 'Pablo';
+    const { nextState, output } = stateTransition(S1, name);
+    const greeting = greet(name, hours);
+
+    it('should transitioning from NAME state to WORD state', () => {
+      expect(nextState.q).to.equal(STATES.WORD);
+    });
+
+    it('should set the name', () => {
+      expect(nextState.attrs.name).to.equal(name);
+    });
+
+    it('should have a single output', () => {
+      expect(output.length).to.equal(1);
+    });
+
+    it('should have a output greeting with name', () => {
+      expect(output).to.be.an('array').that.does.include(greeting);
+    });
+
   });
 
 });
