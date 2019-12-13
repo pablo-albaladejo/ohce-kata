420b74cd810674b1a4a915a68ba2013972cca086
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-12-13 16:28:33 +0100

    e2e test: greetings red

--- test/e2e/cli.js
+++ test/e2e/cli.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai').use(require('chai-string'));
 const cmd = require('../helpers/cmd');
+const {greet } = require('../../lib/strings');
 
 let CLI;
-
 describe('it tests cli', () => {
 
     before(() => {
@@ -13,5 +13,19 @@ describe('it tests cli', () => {
         const promiseFromChildProcess = CLI('');
         const output = await promiseFromChildProcess;
         expect(output).not.to.equal(null);
+    });
+
+    it('Should greet the user', async () => {
+        const name = 'Pablo';
+        const hours = 0;
+
+        const promiseFromChildProcess = CLI('-h ' + hours);
+        const childProcess = promiseFromChildProcess.relatedProcess;
+
+        childProcess.send({ name });
+
+        const output = await promiseFromChildProcess;
+        expect(output).to.startsWith(greet(name, hours));
+
     });
 })
