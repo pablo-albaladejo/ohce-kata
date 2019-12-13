e5441ca48ffe12ec94667c7bdf0baab0de7bafa4
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-12-13 12:43:53 +0100

    word state transitions: red failing functions

--- test/transitions.js
+++ test/transitions.js
@@ -2,7 +2,8 @@ const expect = require('expect.js');
 
 const { STATES } = require('../lib/state/states');
 const { stateTransition } = require('../lib/state/transitions');
-const { greet } = require('../lib/strings');
+const { greet, goodbye, cheer } = require('../lib/strings');
+const { reverseStr } = require('../lib/commons');
 
 describe('it tests state transitions', () => {
 
@@ -91,4 +92,82 @@ describe('it tests state transitions', () => {
 
   });
 
+
+  describe('transitions from WORD', () => {
+
+    const S2 = {
+      q: STATES.WORD,
+      attrs: {
+        hours: 0,
+        name: 'Pablo'
+      }
+    }
+
+    const non_palindrome_word = 'non_palindrome_word';
+    const palindrome_word = 'oso';
+    const exit_commnand = 'Stop!';
+
+    describe('transitions from WORD with non palindrome input', () => {
+
+      const reverse_non_palindrome_word = reverseStr(non_palindrome_word);
+      const { nextState, output } = stateTransition(S2, non_palindrome_word);
+
+      it('should no transitioning from WORD state', () => {
+        expect(nextState.q).to.equal(S2.q);
+      });
+
+      it('should have a single output', () => {
+        expect(output.length).to.equal(1);
+      });
+
+      it('should have a output message with the reverse word', () => {
+        expect(output).to.be.an('array').contain(reverse_non_palindrome_word)
+      });
+
+    });
+
+    describe('transitions from WORD with palindrome input', () => {
+
+      const reverse_palindrome_word = reverseStr(palindrome_word);
+      const cheering = cheer();
+      const { nextState, output } = stateTransition(S2, palindrome_word);
+
+      it('should no transitioning from WORD state', () => {
+        expect(nextState.q).to.equal(S2.q);
+      });
+
+      it('should have a double output', () => {
+        expect(output.length).to.equal(2);
+      });
+
+      it('should have a first output message with the reverse word', () => {
+        expect(output[0]).to.be(reverse_palindrome_word);
+      });
+
+      it('should have a second output message with cheering', () => {
+        expect(output[1]).to.be(cheering);
+      });
+
+    });
+
+    describe('transitions from WORD with exit command', () => {
+
+      const goodbye_msg = goodbye(S2.attrs.name);
+      const { nextState, output } = stateTransition(S2, exit_commnand);
+
+      it('should no transitioning from WORD state', () => {
+        expect(nextState.q).to.equal(S2.q);
+      });
+
+      it('should have a single output', () => {
+        expect(output.length).to.equal(1);
+      });
+
+      it('should have a output goodbye message with name', () => {
+        expect(output).to.be.an('array').contain(goodbye_msg)
+      });
+
+    });
+
+  });
 });
